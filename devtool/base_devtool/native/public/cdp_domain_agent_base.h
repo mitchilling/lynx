769fdc15baf7ec3d28da842866f82d694df6df31
ci_lynx <ci_lynx@users.noreply.github.com>
2025-05-07 15:10:49 +0800

    [Feature] Support DevTool module
    Export the symbol in base devtool to support DevTool module.
    doc: https://github.com/lynx-family/lynx/issues/996

--- devtool/base_devtool/native/public/cdp_domain_agent_base.h
+++ devtool/base_devtool/native/public/cdp_domain_agent_base.h
@@ -7,6 +7,7 @@
 #include <memory>
 #include <string>
 
+#include "devtool/base_devtool/native/public/base_devtool_export.h"
 #include "devtool/base_devtool/native/public/message_sender.h"
 
 namespace lynx {
@@ -17,7 +18,7 @@ namespace devtool {
  * and add it to DevToolAgent. You can implement it specifically referring to
  *  CDPDomainAgentExample.
  */
-class CDPDomainAgentBase {
+class BASE_DEVTOOL_EXPORT CDPDomainAgentBase {
  public:
   virtual ~CDPDomainAgentBase() = default;
   virtual void CallMethod(const std::shared_ptr<MessageSender>& sender,
