435723824edd29e949c33c1c6d1aaf782f521a5c
ci_lynx <ci_lynx@users.noreply.github.com>
2025-10-20 16:13:27 +0800

    [Refactor] remove the jsi binding of SystemInfo and make it mutable
    This is the first commit to add multi-screen support for Lynx Desktop.
    
    issue: m-6735683773

--- core/runtime/bindings/jsi/global.cc
+++ core/runtime/bindings/jsi/global.cc
@@ -5,17 +5,46 @@
 #include "core/runtime/bindings/jsi/global.h"
 
 #include <memory>
+#include <utility>
 
 #include "base/include/log/logging.h"
 #include "core/inspector/console_message_postman.h"
+#include "core/renderer/tasm/config.h"
+#include "core/renderer/utils/base/tasm_utils.h"
 #include "core/renderer/utils/lynx_env.h"
 #include "core/runtime/bindings/jsi/big_int/jsbi.h"
 #include "core/runtime/bindings/jsi/console.h"
-#include "core/runtime/bindings/jsi/system_info.h"
 #include "core/runtime/bindings/jsi/text_codec_helper.h"
+#include "core/runtime/common/utils.h"
 
 namespace lynx {
 namespace piper {
+
+static piper::Value GetSystemInfo(Runtime& rt) {
+  BASE_STATIC_STRING_DECL(kOSVersion, "osVersion");
+  BASE_STATIC_STRING_DECL(kRuntimeType, "runtimeType");
+  const char* runtime_type = "";
+  switch (rt.type()) {
+    case JSRuntimeType::v8:
+      runtime_type = "v8";
+    case JSRuntimeType::jsc:
+      runtime_type = "jsc";
+    case JSRuntimeType::quickjs:
+      runtime_type = "quickjs";
+    case JSRuntimeType::jsvm:
+      runtime_type = "jsvm";
+  }
+
+  lepus::Value system_info = tasm::GenerateSystemInfo(nullptr);
+
+  // Properties only for BTS
+  system_info.SetProperty(kOSVersion,
+                          lepus::Value(tasm::Config::GetOsVersion()));
+  system_info.SetProperty(kRuntimeType, lepus::Value(runtime_type));
+
+  auto js_value = valueFromLepus(rt, system_info);
+  return std::move(*js_value);
+}
 
 Global::~Global() { LOGI("lynx ~Global()"); }
 
@@ -35,9 +64,8 @@ void Global::Init(std::shared_ptr<Runtime>& runtime,
       *js_runtime_, std::make_shared<Console>(post_man, page_options));
   global.setProperty(*js_runtime_, "nativeConsole", console_obj);
 
-  Object system_info_obj = Object::createFromHostObject(
-      *js_runtime_, std::make_shared<SystemInfo>());
-  global.setProperty(*js_runtime_, "SystemInfo", system_info_obj);
+  piper::Value system_info = GetSystemInfo(*js_runtime_);
+  global.setProperty(*js_runtime_, "SystemInfo", std::move(system_info));
 
   Object jsbi_obj =
       Object::createFromHostObject(*js_runtime_, std::make_shared<JSBI>());
